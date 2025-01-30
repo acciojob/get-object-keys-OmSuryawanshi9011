c48fe3ff048b0d3d812cc037b6e216db9ec7f55a
OmSuryawanshi9011 <131766249+OmSuryawanshi9011@users.noreply.github.com>
2025-01-30 23:28:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
-// Create a JavaScript object called student
-let student = {
-    name: "John Doe"
+const student = {
+  name: "John Doe",
+  age: 20,
+  grade: "A"
 };
 
-// Define getKeys as a standalone function that accepts an object
+// Function to get all keys of an object
 function getKeys(obj) {
-    return Object.keys(obj); // Return the keys of the passed object
+  return Object.keys(obj);
 }
 
-// Test the function by passing the student object to it
-console.log(getKeys(student)); // Should print [ 'name' ]// Create a JavaScript object called student
-let student = {
-    name: "John Doe"
-};
-
-// Define getKeys as a standalone function that accepts an object
-function getKeys(obj) {
-    return Object.keys(obj); // Return the keys of the passed object
-}
-
-// Test the function by passing the student object to it
-console.log(getKeys(student)); // Should print [ 'name' ]
+// Test the function
+console.log(getKeys(student));
